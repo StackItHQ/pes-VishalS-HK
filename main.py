7e28cb58c56662eb96a82fac9401977a81e3c7a4
Vishal S <vishals@Vishals-MacBook-Air.local>
2024-09-16 14:56:48 +0530

    got the 'CR' of CRUD to work

--- main.py
+++ main.py
@@ -38,11 +38,95 @@ def get_google_sheets_service():
     return build("sheets", "v4", credentials=creds)
 
 def get_db_connection():
-    pass
+    try:
+        connection = mysql.connector.connect(**DB_CONFIG)
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print(f"Error while connecting to the DB: {e}")
+    return None
+
+def get_sheet_data(service):
+    try:
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+        return result.get("values: ", [])
+    except HttpError as error:
+        print(f"An error occured in getting the sheet data: {error}")
+    return []
+
+def update_db_data(db_connection, formatted_data):
+    try:
+        cursor = db_connection.cursor()
+        sql = """insert into spreadsheet (srn, name, semester, dept)
+        values (%s, %s, %s, %s)
+        on duplicate key update
+        name = values(name), semester = values(semester), dept = values(dept)
+        """
+        cursor.executemany(sql, formatted_data)
+        db_connection.commit()
+    except Error as e:
+        print(f"Error while updating database: {e}")
+
+def update_sheet_data(sheet_service, data_to_update):
+    try:
+        body = {"values": data_to_update}
+        sheet_service.spreadsheets().values().update (
+            spreadsheet_id = SPREADSHEET_ID, range = RANGE_NAME,
+            valueInputOption="USER_ENTERED", body = body).execute()
+    except Error as err:
+        print(f"An error occurred: {err}")
+
+
+def sync_sheet_to_database(sheet_data, db_connection):
+    if sheet_data and sheet_data[0][0] == 'SRN':
+        sheet_data = sheet_data[1:]
+    
+    formatted_data = [(row[0], row[1], row[2], row[3]) for row in sheet_data]
+    update_db_data(db_connection, formatted_data)
+
+def get_db_data(db_connection):
+    
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute("select * from spreadsheet")
+        return cursor.fetchall()
+    except Error as e:
+        print(f"Error while fetching data from database: {e}")
+        return []
+
+def sync_database_to_sheet(db_data, sheets_service):
+    data_to_update = [['SRN', 'Name','Semester','Department']] + db_data
+    update_sheet_data = (sheets_service, data_to_update)
+
 
 def main():
     sheets_service = get_google_sheets_service()
     db_connection = get_db_connection()
+
+    if not db_connection:
+        print("Failed to connect to the database!!!")
+        return
+
+    try:
+        while True:
+            ## Syncing Google sheets to database
+            sheet_data = get_sheet_data(sheets_service)
+            sync_sheet_to_database(sheet_data, db_connection)
+
+            ## Syncing database to Google Sheets
+            db_data = get_db_data(db_connection)
+            sync_database_to_sheet(db_data, sheets_service)
+
+            print("Sync complete!")
+            time.sleep(30) # A wait time of 30 seconds before every sunc
+
+    except KeyboardInterrupt as key_err:
+        print(f"{key_err}")
+    finally:
+        if db_connection.is_connected():
+            db_connection.close()
+
 
 
 if __name__ == "__main__":
